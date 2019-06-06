f874d9a1182b624239ba2919a5cba3e91ea3bb69
Honza Charamza <jenda60@gmail.com>
2019-06-06 17:55:47 +0200

    Jednoduchá komunikace

--- src/game.js
+++ src/game.js
@@ -1,9 +1,15 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
+const socket = new WebSocket("ws:127.0.0.1:8080/karel");
 
 const setCanvasDimensions = () => {
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
+};
+
+socket.onopen = () => {
+  socket.send("Smrdíš");
+  socket.onmessage = event => console.log("Dostal jsi zprávu od serveru: ", event.data);
 };
 
 setCanvasDimensions();
--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+const ws = require("express-ws")(app);
+
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "web/index.html")));
 app.get("/main.css", (req, res) => res.sendFile(path.join(__dirname, "web/main.css")));
 app.get("/game.js", (req, res) => res.sendFile(path.join(__dirname, "game.js")));
 
 app.listen(port, () => console.log("Server běží"));
+
+app.ws("/karel", (ws, req) => {
+  ws.on("message", msg => {
+    console.log(msg);
+    ws.send("Zpráva zpět");
+  });
+});
