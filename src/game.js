e9a791827e4d66bb745a4407e0219c6f9eac81d2
Honza Charamza <jenda60@gmail.com>
2019-06-06 17:43:25 +0200

    Základní smyčka a vykreslování

--- src/game.js
+++ src/game.js
@@ -1,1 +1,27 @@
-console.log("Karel");
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const setCanvasDimensions = () => {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+};
+
+setCanvasDimensions();
+window.onresize = setCanvasDimensions;
+
+function loop() {
+  update();
+  render();
+  window.requestAnimationFrame(loop);
+}
+
+function update() {
+
+}
+
+function render() {
+  ctx.fillStyle = "red";
+  ctx.fillRect(20, 20, 80, 80);
+}
+
+window.requestAnimationFrame(loop);
--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@ const port = 8080;
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "web/index.html")));
 app.get("/main.css", (req, res) => res.sendFile(path.join(__dirname, "web/main.css")));
 app.get("/game.js", (req, res) => res.sendFile(path.join(__dirname, "game.js")));
+
 app.listen(port, () => console.log("Server běží"));
